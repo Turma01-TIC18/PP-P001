2153132a8bebfd32f313e58dbdd0a74f41f4c6bb
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-11-26 18:12:17 -0300

    feat:ajustando menu e validação do código

--- Semana3/main.py
+++ Semana3/main.py
@@ -4,17 +4,21 @@ def main():
     supermercado = Supermercado()
 
     while True:
-        print("\nMenu:")
+        print("\n*********> Menu <*********")
         print("1. Inserir novo produto")
         print("2. Excluir produto")
         print("3. Listar todos os produtos")
         print("4. Consultar preço")
         print("5. Sair")
+        print("Digite uma opção (1-5): ")
 
         opcao = input("Escolha uma opção (1-5): ")
 
         if opcao == '1':
             codigo = input("Digite o código do produto (13 dígitos): ")
+            if(codigo < 13):
+                print("O código deve ser de 13 dígitos!")
+                pass
             nome = input("Digite o nome do produto: ")
             preco = float(input("Digite o preço do produto: "))
             supermercado.inserir_produto(codigo, nome, preco)
