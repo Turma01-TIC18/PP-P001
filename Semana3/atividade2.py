9cf02d2c521ddf1fb348a78730ab33241114f9ef
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-11-26 18:50:43 -0300

    feat:ajuste fino nos relatórios, mensagens e excessões

--- Semana3/atividade2.py
+++ Semana3/atividade2.py
@@ -5,53 +5,68 @@ def Reajusta_dez_porcento(lista_empregados):
         empregado['salario'] *= 1.10
 
 def main():
-
-    try:
-         while True:
-            print("\n*********> Menu <*********")
+    while True:
+        try:
+            print("\n\n*********> Menu <*********")
+            print("-------------------------------------------------------------")
             print("1. Ler dados")
             print("2. Listar dados")
             print("3. Reajustar em 10%")
             print("4. Mostrar Lista Reajustada")
             print("5. Sair")
+            print("-------------------------------------------------------------")
             opcao = input("Digite uma opção (1-5): ") 
         
             if opcao == '1':
                 with open('funcionarios.json', 'r') as arquivo:
                     lista_empregados = json.load(arquivo)
                     if open:
+                        print("-------------------------------------------------------------")
                         print("Dados Lidos com Sucesso!")
                     
             elif opcao == '2':
+                print("-------------------------------------------------------------")
                 print("Informações dos empregados antes do reajuste:")
+                print("-------------------------------------------------------------")
                 for empregado in lista_empregados:
                     print(empregado)
 
             elif opcao == '3':
                 Reajusta_dez_porcento(lista_empregados)
+                print("-------------------------------------------------------------")
+                print("Reajuste realizado com Sucesso!!")
+                print("-------------------------------------------------------------")
+
 
             elif opcao == '4':
-                print("\nInformações dos empregados após o reajuste:")
+                print("-------------------------------------------------------------")
+                print("Informações dos empregados após o reajuste:")
+                print("-------------------------------------------------------------")
                 for empregado in lista_empregados:
                     print(empregado)
             elif opcao == '5':
                 print(" ")
                 print("Programa Encerrado com Sucesso!")
+                print("-------------------------------------------------------------")
                 break
             else:
                 print("-------------------------------------------------------------")
                 print("Opção inválida. Tente novamente.")
 
-        # Atualização do arquivo com as informações após o reajuste
+            # Atualização do arquivo com as informações após o reajuste
             with open('funcionarios.json', 'w') as arquivo:
                 json.dump(lista_empregados, arquivo, indent=2)
 
-    except FileNotFoundError:
-        print("Arquivo 'funcionarios.json' não encontrado.")
-    except json.JSONDecodeError:
-        print("Erro ao decodificar o arquivo JSON.")
-    except Exception as e:
-        print(f"Ocorreu um erro: {e}")
+        except FileNotFoundError:
+            print("-------------------------------------------------------------")
+            print("Arquivo 'funcionarios.json' não encontrado.")
+        except json.JSONDecodeError:
+            print("-------------------------------------------------------------")
+            print("Erro ao decodificar o arquivo JSON.")
+        except Exception as e:
+            print("-------------------------------------------------------------")
+            print(f"Ocorreu um erro: {e}")
+        
 
 if __name__ == "__main__":
     main()
